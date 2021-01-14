c6c45a8d8b357df4b4df9b828286f66046daa906
aurelia Segarra <av.segarra@gmail.com>
2021-01-14 16:32:20 +0100

    script js

---
+++ js/app.js
@@ -0,0 +1,74 @@
+const apiKey = "51bc7bf8dab28412d8d6b3722a3bab8a";
+let units = 'metric';
+let searchMethod;
+
+function getSearchMethod(searchTerm) {
+    if(searchTerm.length === 5 && Number.parseInt(searchTerm) + '' === searchTerm)
+        searchMethod ='zip';
+        else 
+        searchMethod = 'q';
+}
+
+function searchWeather (searchTerm) {
+    getSearchMethod(searchTerm);
+    fetch(`http://api.openweathermap.org/data/2.5/weather?${searchMethod}=${searchTerm}&APPID=${apiKey}&units=${units}`)
+    .then(result => {
+        return result.json();
+            }).then(result => {
+                 init(result);
+    })
+};
+
+function init(resultFromServer) {
+    console.log(resultFromServer)
+    switch (resultFromServer.weather[0].main) {
+        case 'Clear':
+            document.body.style.backgroundImage = 'url("images/clear.jpg")';
+            break;
+        case 'Clouds':
+            document.body.style.backgroundImage = 'url("images/clear.jpg")';
+            break;
+        case 'Rain':
+            document.body.style.backgroundImage = 'url("images/rainy.jpg")';
+            break;
+        case 'Drizzle':
+            document.body.style.backgroundImage = 'url("images/clear.jpg")';
+            break;
+        case 'Thunderstorm':
+            document.body.style.backgroundImage = 'url("images/storm.jpg")';
+            break;
+        case 'Mist':
+            document.body.style.backgroundImage = 'url("images/snow.jpg")';
+            break;
+        default:
+            document.body.style.backgroundImage = 'url("images/default.jpg")';
+            break;
+    }
+    let weatherDescriptionHeader = document.getElementById('weatherDescriptionHeader');
+    let temperatureElement = document.getElementById('temperature');
+    let humidityElement = document.getElementById('humidity');
+    let windSpeedElement = document.getElementById('windSpeed');
+    let cityHeader = document.getElementById('cityHeader');
+    let weatherIcon = document.getElementById('documentIconImg');
+    
+    weatherIcon.src = 'http://openweathermap.org/img/w/' + resultFromServer.weather[0].icon + '.png';
+    
+    let resultDescription = resultFromServer.weather[0].description;
+    weatherDescriptionHeader.innerText = resultDescription.charAt(0).toUpperCase() + resultDescription.slice(1);
+
+    temperatureElement.innerHTML = Math.floor(resultFromServer.main.temp) + '&#176';
+    windSpeedElement.innerHTML = 'Winds at ' + Math.floor(resultFromServer.wind.speed) + 'm/s';
+    cityHeader.innerHTML = resultFromServer.name;
+    humidityElement.innerHTML = 'Humidity levels at ' + resultFromServer.main.humidity + '%';
+
+    
+};
+
+
+document.getElementById('searchBtn').addEventListener('click', () => {
+    let searchTerm = document.getElementById('searchInput').value;
+    if(searchTerm) {
+        searchWeather(searchTerm);
+    }
+});
+
